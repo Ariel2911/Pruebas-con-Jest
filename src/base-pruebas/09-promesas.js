407822b701bbe6e5bf360e4e28189c1d9b3b346c
Ariel Barrios <ariel29111978@hotmail.com>
2023-08-28 13:24:20 -0300

    Pruebas en 09-promesas con .then y .catch

--- src/base-pruebas/09-promesas.js
+++ src/base-pruebas/09-promesas.js
@@ -1,4 +1,4 @@
-import { getHeroeById } from './08-imp-exp;
+import { getHeroeById } from './08-imp-exp';
 
 export const getHeroeByIdAsync = ( id ) => {
 
@@ -9,12 +9,8 @@ export const getHeroeByIdAsync = ( id ) => {
             if ( p1 ) {
                 resolve( p1 );
             } else {
-                reject( 'No se pudo encontrar el héroe' );
+                reject( 'No se pudo encontrar el héroe ' + id );
             }
         }, 1000 )    
     });
 }
-
-getHeroeByIdAsync(1)
-    .then( console.log )
-    .catch( console.warn );
